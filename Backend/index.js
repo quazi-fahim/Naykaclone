09138b4f30ba7c6d07e5172ce69deb42b1b6e91d
Quazi fahim <fahimahmad.4388@gmail.com>
2024-12-09 20:31:11 +0530

    backttend

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,9 @@ const app=express();
 const connect=require("./config/db")
 const cors = require("cors");
 const userRouter=require("./Controller/User.routes");
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
 
 
 app.use(express.json());
